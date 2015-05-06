f4ff4375c44db0a327a99adc1c09ab49a55a6b92
thissucks <brenden.adamczak@mymail.champlain.edu>
2015-05-06 02:10:47 +0000

    got cards to work and send data between each other

--- main.js
+++ main.js
@@ -3,30 +3,15 @@ var conn;
 //peer is local person
 var peer = new Peer({key: 'obug60hpyawwb3xr'});
 
-//when i create my connection
-peer.on('open', function(id){
-    $('#pid').text(id);
-});
 
-//when i receive data
-peer.on('connection', function(conn) {
-  conn.on('data', function(data){
-    console.log(data);
-  });
-});
-
-//when clicked it takes the value from connectWith input and connect with peer.
-$('#connectWithButton').click(function(){
-    conn = peer.connect($('#connectWith').val());
-    conn.on('open', function(){
-      cards.create();
-      conn.send('hi!');
-    });
-});
-
-
-var cards = {
-    create: function(){
+var cards = function(){
+    //your deck
+    var playersDeck = new Array();
+    
+    /*
+    creates the deck and shuffles it
+    */
+    var createShuffledDeck =  function(){
         var values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
         var types = ['&hearts;','&diams;','&clubs;','&spades;'];
         var deck = new Array();;
@@ -39,11 +24,77 @@ var cards = {
         for(i = deck.length; i != 0; i--){
             var index = Math.floor(Math.random() * (deck.length));
             shuffledDeck.push(deck[index]);
-            console.log(deck[index]);
             deck.splice(index,1);
         }
-        for(i = shuffledDeck.length; i != 0; i-=2){
-            conn.send(shuffledDeck[i]);
+        return shuffledDeck;
+    };
+    /*
+    takes conn and playerDeck and gives each there cards
+    */
+    var spreadCardOut = function(){
+        var deck = createShuffledDeck();
+        for(var i = deck.length; i != 0; i-=2){
+            playersDeck.push(deck[i])
+            conn.send({card: deck[i-1]});
         }
+    };
+    
+    var removeTopCard = function(){
+        var firstCard = playersDeck[0];
+        playersDeck.shift(); //removes first card
+        conn.send({flipCard: firstCard});
+        return firstCard;
+    };
+    var addCard = function(value){
+        playersDeck.push(value);
     }
-}
+    
+    return{
+        newGame:spreadCardOut,
+        removeTopCard:removeTopCard,
+        addCard:addCard
+    };
+}();
+
+
+//when i create my connection
+peer.on('open', function(id){
+    $('#pid').text(id);
+});
+
+//when i receive data
+peer.on('connection', function(othersConn) {
+  if(typeof conn === 'undefined'){
+    conn = peer.connect(othersConn.peer);
+    console.log("asdfasdf");
+  }
+  console.log("connectiong");
+  othersConn.on('data', function(data){
+    if(data.hasOwnProperty('card')){
+        cards.addCard(data.card)
+        console.log(data)
+    }
+    else if(data.hasOwnProperty('flipCard')){
+        $('#opponentsCard').html(data.flipCard);
+    }
+    else{
+        console.log(data);
+    }
+  });
+});
+
+//when clicked it takes the value from connectWith input and connect with peer.
+$('#connectWithButton').click(function(){
+    conn = peer.connect($('#connectWith').val());
+    conn.on('open', function(){
+      cards.newGame();
+      conn.send('hi!');
+    });
+});
+
+$('#flipCard').click(function(){
+    $('#playersCard').html(cards.removeTopCard());
+});
+
+
+
